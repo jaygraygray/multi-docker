063ebcab0b767b9c7ce28da986fde801dd1f451f
jaygraygray <jeremygray001@gmail.com>
2021-01-30 14:22:03 -0500

    initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: '',
+  }
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const { data: values } = await axios.get('/api/values/current');
+    this.setState({ values })
+  }
+
+  async fetchIndexes() {
+    const { data: seenIndexes } = await axios.get('/api/values/all');
+    this.setState({ seenIndexes })
+  }
+
+  handleSubmit = async (ev) => {
+    ev.preventDefault();
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+
+    this.setState({ index: '' });
+  }
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ')
+  }
+
+  renderCalculatedValues() {
+    const entries = [];
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key}, I calculated {this.state.values[key]}
+        </div>
+      )
+    }
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter Index</label>
+          <input
+            value={this.state}
+            onChange={e => this.stateState({ index: e.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Seen Indexes</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated Values</h3>
+        {this.renderCalculatedValues()}
+
+      </div>
+    )
+  }
+}
+
+export default Fib;
+
---
+++ client/src/Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page = () => {
+  return (
+    <div>
+      Another page, babes
+      <Link to="/">Clicky</Link>
+    </div>
+  )
+}
+
+export default Page;
